f2799237019ca8b132bc9ccaa3a9a6bae1b57d68
Andrija233 <andrijam007@hotmail.rs>
2024-04-12 01:42:37 +0200

    Products updated

--- src/pages/LoginSignup.jsx
+++ src/pages/LoginSignup.jsx
@@ -1,9 +1,23 @@
 import React from 'react'
+import './CSS/LoginSignup.css'
 
 const LoginSignup = () => {
   return (
-    <div>
-      
+    <div className='login-signup'>
+      <div className="loginsignup-container">
+        <h1>Sign Up</h1>
+        <div className="loginsignup-fields">
+          <input type="text" placeholder='Your Name' />
+          <input type="email" placeholder='Email Address' />
+          <input type="password" placeholder='Password' />
+        </div>
+        <button>Continue</button>
+        <p className='loginsignup-login'>Already have an account? <span>Login here</span></p>
+        <div className="loginsignup-agree">
+          <input type="checkbox" name='' id='' />
+          <p>I agree to the Terms of Service & Privacy Policy</p>
+        </div>
+      </div>
     </div>
   )
 }
--- src/pages/Product.jsx
+++ src/pages/Product.jsx
@@ -1,9 +1,18 @@
 import React from 'react'
+import { ShopContext } from '../context/ShopContext'
+import { useContext } from 'react'
+import { useParams } from 'react-router-dom'
+import Breadcrum from '../components/BreadCrums/Breadcrum'
+import ProductDisplay from '../components/ProductDisplay/ProductDisplay'
 
 const Product = () => {
+  const {all_product} = useContext(ShopContext)
+  const {productId} = useParams();
+  const product = all_product.find((item) => item.id === Number(productId));
   return (
     <div>
-      
+      <Breadcrum product={product}/>
+      <ProductDisplay product={product} />
     </div>
   )
 }
